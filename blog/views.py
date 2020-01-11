56d38692914c103895a3f8ef052d7f0544707fdc
KlevakinSergey <klevakin.sergey@gmail.com>
2020-01-11 15:50:38 +0600

    Template Extension

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,6 @@ from .models import Post
 
 
 def index(request):
-
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')  
     return render(request, 'index.html' , {'posts': posts})
 
